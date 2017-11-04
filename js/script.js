50d74e3597a26146732dee3340dac8b62f14223a
thezaff <zkhodjaev@wiut.uz>
2017-11-04 15:54:36 +0500

    whatever again

--- js/script.js
+++ js/script.js
@@ -1,45 +1,92 @@
-// var navbar = document.getElementsById('navbar');
-// var sticky = navbar.offsetTop;
-
-// function stickNavbar() {
-//     if (window.pageYOffset >= sticky) {
-//         navbar.classList.add("sticky")
-//       } else {
-//         navbar.classList.remove("sticky");
-//       }
-// }
-
-
-
-// $('#addPet').click(() => {
-
-//     var $name = $('#name');
-//     var $species = $('#species');
-//     var $notes = $('#notes');
-
-//     var $newPet = '<div class="container newPet">' + '<b>' + $name.val() +':</b> ' + $species.val() + '</br>' + $notes.val() +
-//     '<button class="btn btn-danger" id="deleteBtn">Remove this object</button> </div>';
-
-//     $('#newPet').append($newPet).css('display', 'none').fadeIn(2000);
-// });
-
-// $('.btn-danger').click(function() {
-//     $(this).parent().remove();
-// });
-
-
-
 var xhr = new XMLHttpRequest();
+xhr.onreadystatechange = function () {
+    if(xhr.readyState === 4) {
+        console.log(typeof xhr.responseText);
+    }
+}
+
+xhr.open('GET', 'data.json');
+xhr.send();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* var xhr = new XMLHttpRequest();
 xhr.onreadystatechange = function() {
     if (xhr.readyState === 4) {
         if (xhr.status === 200) {
             document.getElementById('ajax').innerHTML = xhr.responseText;
-        } else {
-            alert(xhr.statusText);
-    }
-}
+        }
+
+};
 
 xhr.open('GET', 'sidebar.html');
 $('h1').click(function() {
     xhr.send();
+    console.log(typeof xhr.responseText);
 });
+ */
+
+// var xhr = new XMLHttpRequest();
+// xhr.onreadystatechange = function() {
+//     if(xhr.readyState === 4) {
+//         console.log(xhr.responseText);
+//     }
+// }
+
+// xhr.open('OPEN', 'data.json')
+// xhr.send();
+
+/* var navbar = document.getElementsById('navbar');
+var sticky = navbar.offsetTop;
+
+function stickNavbar() {
+    if (window.pageYOffset >= sticky) {
+        navbar.classList.add("sticky")
+      } else {
+        navbar.classList.remove("sticky");
+      }
+}
+
+
+
+$('#addPet').click(() => {
+
+    var $name = $('#name');
+    var $species = $('#species');
+    var $notes = $('#notes');
+
+    var $newPet = '<div class="container newPet">' + '<b>' + $name.val() +':</b> ' + $species.val() + '</br>' + $notes.val() +
+    '<button class="btn btn-danger" id="deleteBtn">Remove this object</button> </div>';
+
+    $('#newPet').append($newPet).css('display', 'none').fadeIn(2000);
+});
+
+$('.btn-danger').click(function() {
+    $(this).parent().remove();
+});
+ */
